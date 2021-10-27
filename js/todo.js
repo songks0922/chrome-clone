70a53c4f288b355b913e90f26ff4edbd2f3b0dcc
Song kyeong seok <skj0922@kookmin.ac.kr>
2021-10-27 13:09:37 +0900

    feat: todo delete 로컬 스토리지 연동

--- js/todo.js
+++ js/todo.js
@@ -2,17 +2,28 @@ const toDoForm = document.getElementById('todo-form');
 const toDoInput = document.querySelector('#todo-form input');
 const toDoList = document.getElementById('todo-list');
 
+const TODOS_KEY = 'todos';
+
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function deleteTodo(event) {
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter((item) => item.id !== parseInt(li.id));
+  saveToDos();
 }
 
-function paintToDO(newTodo) {
+function paintToDo(newTodo) {
   const li = document.createElement('li');
+  li.id = newTodo.id;
   const span = document.createElement('span');
   const button = document.createElement('button');
 
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   button.innerText = '❌';
   button.addEventListener('click', deleteTodo);
 
@@ -25,7 +36,21 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = '';
-  paintToDO(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
 }
 
 toDoForm.addEventListener('submit', handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
